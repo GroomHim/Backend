3bedef7bcf554dcd6b464ed517f1949c2fc8c555
YGwan <good0619@naver.com>
2024-08-06 00:43:48 +0900

    feat : set env

---
+++ src/test/java/groom/him/GroomhimApplicationTests.java
@@ -0,0 +1,13 @@
+package groom.him;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomhimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
