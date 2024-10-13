bd8b1903ebe920580a60c92570cce32ad03594aa
seoeunbae <enyoupark@gmail.com>
2024-10-13 11:55:05 +0900

    [Feat] : spring version 3.3.0 업그레이드

--- src/main/java/groom/him/core/config/SecurityConfig.java
+++ src/main/java/groom/him/core/config/SecurityConfig.java
@@ -1,75 +1,89 @@
 package groom.him.core.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+
 @Configuration
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 //    TODO: jwt token provider, auth service 작성
 //    private final JwtTokenProvider jwtTokenProvider;
 //    private final AuthService authService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        //rest api
-        http.httpBasic().disable()
-                .cors()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .csrf().disable();
-
-        // TODO: API명세서 작성 후, authenticated path 인가 필요
-        http.authorizeRequests()
-                .antMatchers("/swagger-ui.html", "/swagger-ui/**","/swagger-resources/**","/v2/api-docs", "/swagger/**", "/webjars/**", "/configuration/ui", "/configuration/security").anonymous()
-                .mvcMatchers(HttpMethod.GET, "/groomhim/v1/health-check").permitAll()
-                .mvcMatchers(HttpMethod.POST, "/groomhim/auth/logout").permitAll()
-                .anyRequest().authenticated();
-
-        // authenticated exception 권한예외처리
-        http.exceptionHandling()
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                .accessDeniedHandler(new AccessDeniedHandler() {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(corsConfigurer-> corsConfigurer.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll() // TODO : 운영 서버에서는 authenticated()로 변경 요함
+                )
+                .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(ex-> ex.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)).accessDeniedHandler(new AccessDeniedHandler() {
                     @Override
                     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-                        // response.sendRedirect("/admin/auth/denied"); // TODO: 접근권한이없음을 알리는 페이지 경로로 리다이렉트
+                        // Custom access denied handler logic
                         throw new java.nio.file.AccessDeniedException("접근 권한이 없습니다.");
                     }
-                });
+                }));
 
+        // TODO: Add JWT authentication filter
+        // http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, authService), UsernamePasswordAuthenticationFilter.class);
 
-        // TODO: jwt auth filter inject
-        // http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider,authService), UsernamePasswordAuthenticationFiltet.class);
+        return http.build();
     }
 
-    // jwtAuthenticationFilter 필터 안 타도록 설정해주기
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/groomhim/auth/sign-in")
-                .antMatchers("/groomhim/auth/refresh-token")
-                .antMatchers("/groomhim/v1/health-check")
-                .antMatchers("/swagger-ui.html/**", "/swagger-ui/**","/swagger-resources/**","/v2/api-docs", "/swagger/**", "/webjars/**", "/configuration/ui", "/configuration/security");
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/swagger-ui.html/**", "/swagger-ui/**", "/swagger-resources/**", "/v1/api-docs", "/swagger/**", "/webjars/**", "/configuration/ui", "/configuration/security")
+                .requestMatchers("/groomhim/auth/sign-in", "/groomhim/auth/refresh-token", "/groomhim/v1/health-check");
     }
+
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:3000")); // ⭐️ 허용할 origin
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
