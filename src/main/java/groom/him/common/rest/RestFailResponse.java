42ea54dc0be19dfbe85fdfa371760ec03ec584e4
seoeunbae <enyoupark@gmail.com>
2024-11-05 12:48:57 +0900

    [Feat] restFailResponse 규격 작성

--- src/main/java/groom/him/common/rest/RestFailResponse.java
+++ src/main/java/groom/him/common/rest/RestFailResponse.java
@@ -3,11 +3,15 @@ package groom.him.common.rest;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class RestFailResponse {
+public class RestFailResponse<T> {
     int statusCode;
     String message;
+    private RestFailResponse(int statusCode, String message){
+        statusCode = statusCode;
+        message = message;
+    }
 
-    private RestFailResponse(){
-
+    public static <T> RestFailResponse<T> newInstance(int statusCode, String message) {
+        return new RestFailResponse<>(statusCode, message);
     }
 }
