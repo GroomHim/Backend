89d2a4604a2203fcf389d1ce17ba2faa9f7f06d9
YooJin919 <jiny08360@gmail.com>
2024-11-12 23:53:54 +0900

    [Feat] FAQ Category Entity 생성

---
+++ src/main/java/groom/him/core/domain/faq/models/entity/FaqCategoryEntity.java
@@ -0,0 +1,39 @@
+package groom.him.core.domain.faq.models.entity;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Table(name = "FAQ_CATEGORY")
+@Entity
+public class FaqCategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT 사용
+    @Column(name = "faq_category_id")
+    private Long faqCategoryId;
+
+    @Column(name = "faq_category_name")
+    private String faqCategoryName;
+
+    @Column(updatable = false, name = "reg_dt")
+    @CreationTimestamp
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime regDt;
+
+    @Column(name = "udt_dt")
+    @UpdateTimestamp
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime udtDt;
+}
