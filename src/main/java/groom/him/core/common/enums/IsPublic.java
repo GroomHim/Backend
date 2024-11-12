cff2cbcb9fdd69eae94eac525162c560a361ff6b
YooJin919 <jiny08360@gmail.com>
2024-11-12 19:33:58 +0900

    [Feat] IsPublic Enum, Converter 생성

--- src/main/java/groom/him/core/common/enums/IsPublic.java
+++ src/main/java/groom/him/core/common/enums/IsPublic.java
@@ -1,2 +1,30 @@
-package groom.him.core.common.enums;public enum IsPublic {
+package groom.him.core.common.enums;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.Arrays;
+
+public enum IsPublic {
+    OPEN(true),
+    CLOSE(false);
+
+    private final Boolean code;
+
+    IsPublic(Boolean code) {
+        this.code = code;
+    }
+
+    @JsonValue
+    public Boolean getCode() {
+        return code;
+    }
+
+    @JsonCreator
+    public static groom.him.core.common.enums.IsPublic fromCode(Boolean code) {
+        return Arrays.stream(groom.him.core.common.enums.IsPublic.values())
+            .filter(v -> v.getCode().equals(code))
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException(String.format("공개여부에 %s가 존재하지 않습니다.", code)));
+    }
 }
