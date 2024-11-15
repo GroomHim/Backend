ad3847417ae0bea6570caf19b2980891c60d8555
seon-dev <baeseoeun1221@gmail.com>
2024-11-15 17:15:51 +0900

    [Chore] 충돌 해결 머지

--- src/main/java/groom/him/core/config/SecurityConfig.java
+++ src/main/java/groom/him/core/config/SecurityConfig.java
@@ -23,13 +23,15 @@ import java.util.Collections;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-//    TODO: jwt token provider, auth service 작성
-//    private final JwtTokenProvider jwtTokenProvider;
-//    private final AuthService authService;
+
     private final String ORIGIN = "http://localhost:3000";
+
     private final String[] FRONT_SRC_URLS = new String[]{"/webjars/**", "/configuration/ui", "/configuration/security"};
+
     private final String[] DOCS_SRC_URLS = new String[]{"/swagger-ui.html/**", "/swagger-ui/**", "/swagger-resources/**", "/v1/api-docs", "/swagger/**","/groomhim/v1/health-check"};
+
     private final String[] AUTH_URLS = new String[] {"/groomhim/auth/sign-in", "/groomhim/auth/refresh-token"};
+
     private final AuthAccessDeniedHandler accessDeniedCustomHandler = AuthAccessDeniedHandler.getInstance();
 
     @Bean
