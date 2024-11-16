b8d3365b9b5e98c388f4aa8040531518fe246ed5
YooJin919 <jiny08360@gmail.com>
2024-11-16 22:25:27 +0900

    [Refactor] FaqEntity @NotNull, @NoArgsConstructor 어노테이션 추가

--- src/main/java/groom/him/domain/faq/models/entity/FaqEntity.java
+++ src/main/java/groom/him/domain/faq/models/entity/FaqEntity.java
@@ -16,15 +16,20 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Getter
 @Table(name = "FAQ")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class FaqEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT 사용
     @Column(name = "faq_id")
@@ -34,16 +39,20 @@ public class FaqEntity {
     @JoinColumn(name = "faq_category_id")
     private FaqCategoryEntity faqCategory;
 
+    @NotNull
     @Column(name = "question", length = 50)
     private String question;
 
+    @NotNull
     @Column(name = "answer", length = 255)
     private String answer;
 
+    @NotNull
     @Column(name = "prio", length = 20)
     private String prio;
 
     @Column(name = "is_public", length = 1)
+    @NotNull
     @Convert(converter = IsPublicConverter.class)
     private IsPublic isPublic = IsPublic.CLOSE;
 
