e0bf8d19510d15304d079a243ce4763b5a17c047
YooJin919 <jiny08360@gmail.com>
2024-11-16 21:45:54 +0900

    [Refactor] NoticeEntity @NotNull, @NoArgsConstructor 어노테이션 추가

--- src/main/java/groom/him/domain/notice/models/entities/NoticeEntity.java
+++ src/main/java/groom/him/domain/notice/models/entities/NoticeEntity.java
@@ -15,37 +15,46 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Getter
 @Table(name = "NOTICE")
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NoticeEntity {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT 사용
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "notice_id")
     private Long noticeId;
 
-//    TODO: MemberEntity 코드 머지 후 수정 예정
+    //    TODO: MemberEntity 코드 머지 후 수정 예정
 //    @ManyToOne
 //    @JoinColumn(name = "member_id")
 //    private MemberEntity member;
     @Column(name = "member_id")
     private Long memberId;
 
+    @NotNull
     @Column(name = "title", length = 20)
     private String title;
 
+    @NotNull
     @Column(name = "content", length = 255)
     private String content;
 
+    @NotNull
     @Column(name = "is_public", length = 1)
     @Convert(converter = IsPublicConverter.class)
     private IsPublic isPublic = IsPublic.CLOSE;
 
+    // TODO: AuditingField 상속하도록 수정
     @Column(updatable = false, name = "reg_dt")
     @CreationTimestamp
     @JsonSerialize(using = LocalDateTimeSerializer.class)
