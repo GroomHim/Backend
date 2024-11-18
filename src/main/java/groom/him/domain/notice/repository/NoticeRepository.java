8a7386a9c06616b6880b096c208b9776fa830685
YooJin919 <jiny08360@gmail.com>
2024-11-18 21:05:13 +0900

    [Chore] 불필요한 개행 제거

--- src/main/java/groom/him/domain/notice/repository/NoticeRepository.java
+++ src/main/java/groom/him/domain/notice/repository/NoticeRepository.java
@@ -8,6 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<NoticeEntity, Long> {
-
     List<NoticeEntity> findAllByIsPublicOrderByRegDtDesc(IsPublic isPublic);
 }
