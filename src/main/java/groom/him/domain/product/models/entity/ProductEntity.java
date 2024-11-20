924f0b871ca608c22307a8313ad46206ff87c3aa
YooJin919 <jiny08360@gmail.com>
2024-11-21 02:14:23 +0900

    [Feat] Product Entity 생성

---
+++ src/main/java/groom/him/domain/product/models/entity/ProductEntity.java
@@ -0,0 +1,55 @@
+package groom.him.domain.product.models.entity;
+
+import groom.him.common.models.entity.AuditingFields;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "PRODUCT")
+@Entity
+public class ProductEntity extends AuditingFields {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Integer productId;
+
+    // TODO: BRAND Entity 매핑
+    @Column(name = "brand_id")
+    private Integer brandId;
+
+    // TODO: CATEGORY Entity 매핑
+    @Column(name = "category_id")
+    private Integer categoryId;
+
+    @NotNull
+    @Column(name = "product_name", length = 50)
+    private String productName;
+
+    @NotNull
+    @Column(name = "price")
+    private Integer price;
+
+    @NotNull
+    @Column(name = "discount_rate")
+    private Float discountRate;
+
+    @NotNull
+    @Column(name = "discounted_price")
+    private Integer discountedPrice;
+
+    @Column(name = "ingredients")
+    private String ingredients;
+
+    @NotNull
+    @Column(name = "img_url", length = 2048)
+    private String imgUrl;
+}
