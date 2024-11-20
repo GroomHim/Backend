2e8a751e5665fc857a8a9b9a3c9891426c8094a2
YooJin919 <jiny08360@gmail.com>
2024-11-21 02:14:59 +0900

    [Feat] ProductSkinTypeLink Entity 생성

---
+++ src/main/java/groom/him/domain/product/models/entity/ProductSkinTypeLinkEntity.java
@@ -0,0 +1,33 @@
+package groom.him.domain.product.models.entity;
+
+import groom.him.common.models.entity.SkinTypeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "PRODUCT_SKIN_TYPE_LINK")
+@Entity
+public class ProductSkinTypeLinkEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_skin_type_id")
+    private Integer productSkinTypeId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private ProductEntity product;
+
+    @ManyToOne
+    @JoinColumn(name = "skin_type_id", nullable = false)
+    private SkinTypeEntity skinType;
+}
