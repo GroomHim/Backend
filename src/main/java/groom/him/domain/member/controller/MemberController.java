a93c7ec769edac4414637d55f076596b52cd2c94
YooJin919 <jiny08360@gmail.com>
2024-11-21 20:49:34 +0900

    [Chore] 사용자 위시리스트 조회 controller method명 변경

--- src/main/java/groom/him/domain/member/controller/MemberController.java
+++ src/main/java/groom/him/domain/member/controller/MemberController.java
@@ -20,7 +20,7 @@ public class MemberController {
     }
 
     @GetMapping("/{memberId}/wish")
-    public Response<List<ProductBriefResponse>> getFaqList(
+    public Response<List<ProductBriefResponse>> findUserWishList(
         @PathVariable("memberId") Integer memberId, @RequestParam("skin-type") Boolean isSkinType) {
         List<ProductBriefResponse> data = memberService.findUserWishList(memberId, isSkinType);
         return Response.success(data);
