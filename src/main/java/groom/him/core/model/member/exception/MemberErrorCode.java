2f45534e05fe250375e3df92651ecbe96d1656cd
YGwan <good0619@naver.com>
2024-11-24 11:45:58 +0900

    [Feat] 패스워드 검증 관련 dto & 에러코드 추가

--- src/main/java/groom/him/core/model/member/exception/MemberErrorCode.java
+++ src/main/java/groom/him/core/model/member/exception/MemberErrorCode.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements HttpErrorCode {
     MEMBER_NOT_EXIST(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
     MEMBER_DUPLICATED(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다."),
-    MEMBER_NOT_VALID(HttpStatus.BAD_REQUEST, "잘못된 형식입니다.");
+    MEMBER_NOT_VALID(HttpStatus.BAD_REQUEST, "잘못된 형식입니다."),
+    MEMBER_INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "잘못된 패스워드입니다.")
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
---
+++ src/main/java/groom/him/domain/member/models/dto/request/ValidatePasswordRequest.java
@@ -0,0 +1,6 @@
+package groom.him.domain.member.models.dto.request;
+
+public record ValidatePasswordRequest(
+    String password
+) {
+}
