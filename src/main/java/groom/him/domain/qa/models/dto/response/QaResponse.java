e114b068c21373cfa57f1db86b0bed722cba23af
YooJin919 <jiny08360@gmail.com>
2024-11-25 21:29:39 +0900

    [Feat] Qa 조회 Response Dto 생성

--- src/main/java/groom/him/domain/qa/models/dto/response/QaResponse.java
+++ src/main/java/groom/him/domain/qa/models/dto/response/QaResponse.java
@@ -1,2 +1,23 @@
-package groom.him.domain.qa.models.dto.response;public record QaResponse() {
+package groom.him.domain.qa.models.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import groom.him.domain.qa.models.enums.QaStatus;
+import java.time.LocalDateTime;
+
+@JsonInclude(Include.NON_NULL)
+public record QaResponse(
+    Integer qaId,
+    String category,
+    String title,
+    String content,
+    String answer,
+    QaStatus status,
+    LocalDateTime regDt
+) {
+
+    public static QaResponse of(Integer qaId, String category, String title, String content,
+        String answer, QaStatus status, LocalDateTime regDt) {
+        return new QaResponse(qaId, category, title, content, answer, status, regDt);
+    }
 }
