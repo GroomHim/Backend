05d687a02fdec45c54e919c2b797d97f5a2808be
YooJin919 <jiny08360@gmail.com>
2024-11-25 19:43:20 +0900

    [Fix] 사용자 위시 조회 memberId @PathVariable -> @AuthenticationPrincipal

--- src/main/java/groom/him/domain/member/controller/MemberController.java
+++ src/main/java/groom/him/domain/member/controller/MemberController.java
@@ -1,11 +1,12 @@
 package groom.him.domain.member.controller;
 
 import groom.him.core.dto.Response;
+import groom.him.domain.member.models.entity.MemberEntity;
 import groom.him.domain.member.service.MemberService;
 import groom.him.domain.product.models.dto.response.ProductBriefResponse;
 import java.util.List;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,10 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping("/{memberId}/wish")
+    @GetMapping("/wish")
     public Response<List<ProductBriefResponse>> findMemberWishList(
-        @PathVariable("memberId") Integer memberId, @RequestParam("skin-type") Boolean isSkinType) {
-        List<ProductBriefResponse> data = memberService.findMemberWishList(memberId, isSkinType);
-        return Response.success(data);
+        @AuthenticationPrincipal MemberEntity member,
+        @RequestParam("skin-type") Boolean isSkinType) {
+        return Response.success(memberService.findMemberWishList(member.getMemberId(), isSkinType));
     }
 }
