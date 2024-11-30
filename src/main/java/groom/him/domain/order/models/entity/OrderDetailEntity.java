b7f9c0ba1d1d655ddd1815fe8cfb73f4acff4abe
YooJin919 <jiny08360@gmail.com>
2024-11-30 22:37:30 +0900

    [Feat] Order, OrderDetail Entity 초안 생성

---
+++ src/main/java/groom/him/domain/order/models/entity/OrderDetailEntity.java
@@ -0,0 +1,42 @@
+package groom.him.domain.order.models.entity;
+
+import groom.him.domain.product.models.entity.ProductEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "ORDER_DETAIL")
+@Entity
+public class OrderDetailEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_detail_id")
+    private Integer orderDetailId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductEntity product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private OrderEntity order;
+
+    @NotNull
+    @Column(name = "price")
+    private Integer price;
+
+    @NotNull
+    @Column(name = "quantity")
+    private Integer quantity;
+}
---
+++ src/main/java/groom/him/domain/order/models/entity/OrderEntity.java
@@ -0,0 +1,39 @@
+package groom.him.domain.order.models.entity;
+
+import groom.him.common.models.entity.RegisterDateFields;
+import groom.him.domain.member.models.entity.MemberEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "ORDER")
+@Entity
+public class OrderEntity extends RegisterDateFields {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Integer orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private MemberEntity member;
+
+    @NotNull
+    @Column(name = "price")
+    private Integer price;
+
+    @NotNull
+    @Column(name = "quantity")
+    private Integer quantity;
+}
