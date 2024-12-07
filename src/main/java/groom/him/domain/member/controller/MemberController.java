5ba0876cf372056b57f896de8f22a0a544cc9626
Yonggwan Park <ygpark@conportlab.com>
2024-12-08 02:08:42 +0900

    [Feat] 내 정보 조회 로직 구현

--- src/main/java/groom/him/domain/member/controller/MemberController.java
+++ src/main/java/groom/him/domain/member/controller/MemberController.java
@@ -1,15 +1,19 @@
 package groom.him.domain.member.controller;
 
 import groom.him.core.dto.Response;
+import groom.him.domain.member.models.dto.request.ModifyMyInfoRequest;
 import groom.him.domain.member.models.dto.request.ModifyPasswordRequest;
 import groom.him.domain.member.models.dto.request.ValidatePasswordRequest;
+import groom.him.domain.member.models.dto.response.MemberResponse;
 import groom.him.domain.member.models.entity.MemberEntity;
 import groom.him.domain.member.service.MemberService;
 import groom.him.domain.product.models.dto.response.ProductBriefResponse;
 import groom.him.domain.qa.models.dto.response.QaResponse;
 import groom.him.domain.qa.models.enums.QaStatus;
+
 import java.time.LocalDate;
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -20,34 +24,40 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService memberService;
 
+    @GetMapping("/info")
+    public Response<MemberResponse> findMyInfo(@AuthenticationPrincipal MemberEntity member) {
+        var response = memberService.findMyInfo(member.getMemberId());
+        return Response.success(response);
+    }
+
     @PatchMapping("/pwd")
     public Response<Integer> modifyPassword(@AuthenticationPrincipal MemberEntity member,
-        @RequestBody ModifyPasswordRequest request) {
+                                            @RequestBody ModifyPasswordRequest request) {
         memberService.modifyPassword(member.getMemberId(), request.newPassword());
         return Response.success();
     }
 
     @PostMapping("/validate/pwd")
     public Response<Integer> validatePassword(@AuthenticationPrincipal MemberEntity member,
-        @RequestBody ValidatePasswordRequest request) {
+                                              @RequestBody ValidatePasswordRequest request) {
         memberService.validatePassword(member.getMemberId(), request.password());
         return Response.success();
     }
 
     @GetMapping("/wish")
     public Response<List<ProductBriefResponse>> findMemberWishList(
-        @AuthenticationPrincipal MemberEntity member,
-        @RequestParam("skin-type") Boolean isSkinType) {
+            @AuthenticationPrincipal MemberEntity member,
+            @RequestParam("skin-type") Boolean isSkinType) {
         return Response.success(memberService.findMemberWishList(member.getMemberId(), isSkinType));
     }
 
     @GetMapping("/qa")
     public Response<List<QaResponse>> findMemberQaList(
-        @AuthenticationPrincipal MemberEntity member,
-        @RequestParam(value = "status", required = false) QaStatus qaStatus,
-        @RequestParam(value = "start-date", required = false) LocalDate startDate,
-        @RequestParam(value = "end-date", required = false) LocalDate endDate) {
+            @AuthenticationPrincipal MemberEntity member,
+            @RequestParam(value = "status", required = false) QaStatus qaStatus,
+            @RequestParam(value = "start-date", required = false) LocalDate startDate,
+            @RequestParam(value = "end-date", required = false) LocalDate endDate) {
         return Response.success(
-            memberService.findMemberQaList(member.getMemberId(), qaStatus, startDate, endDate));
+                memberService.findMemberQaList(member.getMemberId(), qaStatus, startDate, endDate));
     }
 }
--- src/main/java/groom/him/domain/member/service/MemberService.java
+++ src/main/java/groom/him/domain/member/service/MemberService.java
@@ -1,20 +1,27 @@
 package groom.him.domain.member.service;
 
+import groom.him.core.dto.Response;
 import groom.him.core.model.member.exception.MemberErrorCode;
 import groom.him.core.model.member.exception.MemberException;
 import groom.him.core.model.member.repository.MemberRepository;
+import groom.him.domain.member.models.dto.request.ModifyMyInfoRequest;
+import groom.him.domain.member.models.dto.response.MemberResponse;
 import groom.him.domain.product.models.dto.response.ProductBriefResponse;
 import groom.him.domain.product.repository.ProductRepository;
 import groom.him.domain.qa.models.dto.response.QaResponse;
 import groom.him.domain.qa.models.enums.QaStatus;
 import groom.him.domain.qa.repository.QaRepository;
 import jakarta.transaction.Transactional;
+
 import java.time.LocalDate;
+
 import groom.him.core.auth.service.AuthService;
 import groom.him.domain.member.models.entity.MemberEntity;
 import groom.him.domain.member.models.entity.data.Password;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +33,11 @@ public class MemberService {
     private final ProductRepository productRepository;
     private final QaRepository qaRepository;
 
+    public MemberResponse findMyInfo(Integer memberId) {
+        MemberEntity member = getMemberById(memberId);
+        return MemberResponse.from(member);
+    }
+
     @Transactional
     public void modifyPassword(Integer memberId, String newPassword) {
         MemberEntity member = getMemberById(memberId);
@@ -35,7 +47,7 @@ public class MemberService {
 
     private MemberEntity getMemberById(Integer memberId) {
         return memberRepository.findById(memberId)
-            .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_EXIST));
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_EXIST));
     }
 
     public void validatePassword(Integer memberId, String password) {
@@ -49,12 +61,12 @@ public class MemberService {
 
     public List<ProductBriefResponse> findMemberWishList(Integer memberId, Boolean isSkinType) {
         return productRepository.findMemberWishProductBriefBySkinType(memberId, isSkinType).stream()
-            .map(ProductBriefResponse::of).collect(Collectors.toList());
+                .map(ProductBriefResponse::of).collect(Collectors.toList());
     }
 
     public List<QaResponse> findMemberQaList(Integer memberId, QaStatus qaStatus,
-        LocalDate startDate, LocalDate endDate) {
+                                             LocalDate startDate, LocalDate endDate) {
         return qaRepository.findQaByMemberIdAndStatusAndRegDt(memberId, qaStatus, startDate,
-            endDate);
+                endDate);
     }
 }
