2f5e42d0059571b8c2f9b0fc9ba9038b115baf90
YooJin919 <jiny08360@gmail.com>
2024-12-08 23:43:12 +0900

    [Feat] Address Exception 생성

---
+++ src/main/java/groom/him/domain/address/models/enums/AddressErrorCode.java
@@ -0,0 +1,20 @@
+package groom.him.domain.address.models.enums;
+
+import groom.him.core.exception.HttpErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AddressErrorCode implements HttpErrorCode {
+    MAX_ADDRESS_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "주소는 최대 10개까지 등록 가능합니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public String getCode() {
+        return name();
+    }
+}
