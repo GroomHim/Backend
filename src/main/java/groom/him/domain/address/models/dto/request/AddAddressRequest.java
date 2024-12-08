1491591539f8b4d89d4cd98da93871f2c5b52cf3
YooJin919 <jiny08360@gmail.com>
2024-12-08 23:16:37 +0900

    [Feat] Address 생성 Request Dto 생성

---
+++ src/main/java/groom/him/domain/address/models/dto/request/AddAddressRequest.java
@@ -0,0 +1,11 @@
+package groom.him.domain.address.models.dto.request;
+
+public record AddAddressRequest(
+    String name,
+    String phoneNumber,
+    String alias,
+    String address,
+    String addressDetail,
+    Boolean isDefault
+) {
+}
