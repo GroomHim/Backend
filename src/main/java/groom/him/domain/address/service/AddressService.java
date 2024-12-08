bc8c7676139cb463558afd1d7db1b1481d0aecac
YooJin919 <jiny08360@gmail.com>
2024-12-08 23:43:40 +0900

    [Feat] Address 생성시 최대 등록 가능개수 초과시 에러 발생

--- src/main/java/groom/him/domain/address/service/AddressService.java
+++ src/main/java/groom/him/domain/address/service/AddressService.java
@@ -1,8 +1,10 @@
 package groom.him.domain.address.service;
 
+import groom.him.domain.address.exception.AddressException;
 import groom.him.domain.address.models.dto.request.AddAddressRequest;
 import groom.him.domain.address.models.dto.response.AddressResponse;
 import groom.him.domain.address.models.entity.AddressEntity;
+import groom.him.domain.address.models.enums.AddressErrorCode;
 import groom.him.domain.address.repository.AddressRepository;
 import groom.him.domain.member.models.entity.MemberEntity;
 import java.util.List;
@@ -15,7 +17,12 @@ import org.springframework.stereotype.Service;
 public class AddressService {
     private final AddressRepository addressRepository;
 
+    private static final Integer MAX_ADDRESS_CNT = 10;
+
     public void addMemberAddress(MemberEntity member, AddAddressRequest request) {
+        if (addressRepository.findAllByMemberId(member.getMemberId()).size() >= MAX_ADDRESS_CNT) {
+            throw new AddressException(AddressErrorCode.MAX_ADDRESS_LIMIT_EXCEEDED);
+        }
         AddressEntity address = new AddressEntity(member, request.name(), request.phoneNumber(),
             request.alias(), request.address(), request.addressDetail(), request.isDefault());
         addressRepository.save(address);
