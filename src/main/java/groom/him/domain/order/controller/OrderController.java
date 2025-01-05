289f42dac6fe094324c764bd774a05345c22e2cb
YGwan <good0619@naver.com>
2025-01-06 02:38:51 +0900

    [Feat] 주문하기 api 구현

--- src/main/java/groom/him/domain/order/controller/OrderController.java
+++ src/main/java/groom/him/domain/order/controller/OrderController.java
@@ -1,11 +1,12 @@
 package groom.him.domain.order.controller;
 
 import groom.him.core.dto.Response;
+import groom.him.domain.member.models.entity.MemberEntity;
+import groom.him.domain.order.models.dto.request.AddOrderRequest;
 import groom.him.domain.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/v1/orders")
 @RequiredArgsConstructor
@@ -18,5 +19,14 @@ public class OrderController {
     public Response<String> getOrderId() {
         String orderNum = orderService.getOrderId();
         return Response.success(orderNum);
+    }
+
+    @PostMapping()
+    public Response<Integer> addOrder(
+        @RequestBody AddOrderRequest request,
+        @AuthenticationPrincipal MemberEntity member
+    ) {
+        orderService.addOrder(member.getMemberId(), request);
+        return Response.success();
     }
 }
