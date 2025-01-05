230ce8454b0cbb1ccd40ea9d1b9178dca772234d
YGwan <good0619@naver.com>
2025-01-06 02:36:33 +0900

    [Feat] 주문 관련 에러 클래스 & code 값 정의

---
+++ src/main/java/groom/him/domain/order/exception/OrderErrorCode.java
@@ -0,0 +1,21 @@
+package groom.him.domain.order.exception;
+
+import groom.him.core.exception.HttpErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum OrderErrorCode implements HttpErrorCode {
+    NOT_ENOUGH_POINTS(HttpStatus.BAD_REQUEST, "포인트가 적습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public String getCode() {
+        return name();
+    }
+}
