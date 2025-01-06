90d82c60c98dff6e561a500f133ee7e01b28159d
YGwan <good0619@naver.com>
2025-01-06 14:00:25 +0900

    [Feat] : BrandEntity 정의

---
+++ src/main/java/groom/him/domain/product/models/entity/BrandEntity.java
@@ -0,0 +1,27 @@
+package groom.him.domain.product.models.entity;
+
+import groom.him.common.models.entity.AuditingFields;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "BRAND")
+@Entity
+public class BrandEntity extends AuditingFields {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "brand_id")
+    private Integer brandId;
+
+    @NotNull
+    @Column(name = "brand_name")
+    String brandName;
+
+    public BrandEntity(String brandName) {
+        this.brandName = brandName;
+    }
+}
