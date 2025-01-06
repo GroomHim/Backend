32fd1772b7a03334032a041dd9de703a5b8232a0
YooJin919 <jiny08360@gmail.com>
2025-01-06 21:19:46 +0900

    [Feat] 전시 카테고리 조회 API 개발

--- src/main/java/groom/him/domain/category/service/ExhibitCategoryService.java
+++ src/main/java/groom/him/domain/category/service/ExhibitCategoryService.java
@@ -1,2 +1,23 @@
-package groom.him.domain.category.service;public class ExhibitCategoryService {
+package groom.him.domain.category.service;
+
+import groom.him.domain.category.models.dto.response.ExhibitCategoryResponse;
+import groom.him.domain.category.repository.ExhibitCategoryRepository;
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExhibitCategoryService {
+    private final ExhibitCategoryRepository exhibitCategoryRepository;
+
+    @Transactional
+    public List<ExhibitCategoryResponse> findExhibitCategoryList() {
+        List<ExhibitCategoryResponse> result = new ArrayList<>();
+        exhibitCategoryRepository.findAllByParentExhibitCategoryIsNull()
+            .forEach(exhibitCategory -> result.add(ExhibitCategoryResponse.of(exhibitCategory)));
+        return result;
+    }
 }
