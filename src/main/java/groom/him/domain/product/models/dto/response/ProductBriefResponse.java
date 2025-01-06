3e172986ccbbdf7ca1c80db4ead344ee70b82cd4
YGwan <good0619@naver.com>
2025-01-06 14:01:27 +0900

    [Feat] : ProductBriefResponse에 brandName 필드 추가

--- src/main/java/groom/him/domain/product/models/dto/response/ProductBriefResponse.java
+++ src/main/java/groom/him/domain/product/models/dto/response/ProductBriefResponse.java
@@ -5,13 +5,15 @@ import groom.him.domain.product.models.entity.ProductEntity;
 public record ProductBriefResponse(
     Integer productId,
     String productName,
+    String brandName,
     Integer price,
     Float discountRate,
     Integer discountedPrice,
     String imgUrl
 ) {
     public static ProductBriefResponse of(ProductEntity product) {
-        return new ProductBriefResponse(product.getProductId(), product.getProductName(),
+        return new ProductBriefResponse(
+            product.getProductId(), product.getProductName(), product.getBrand().getBrandName(),
             product.getPrice(), product.getDiscountRate(), product.getDiscountedPrice(),
             product.getImgUrl()
         );
