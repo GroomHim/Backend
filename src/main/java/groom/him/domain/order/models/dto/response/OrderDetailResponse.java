29299ea4cb546b5d65a8e2c02ac9d77c582cab4d
YGwan <good0619@naver.com>
2025-01-12 00:57:38 +0900

    [Refactor] : Order 관련 response 네임 변경

--- src/main/java/groom/him/domain/order/models/dto/response/OrderDetailResponse.java
+++ src/main/java/groom/him/domain/order/models/dto/response/OrderDetailResponse.java
@@ -1,28 +1,30 @@
-package groom.him.domain.order.models.dto.request;
+package groom.him.domain.order.models.dto.response;
 
 import groom.him.domain.order.models.entity.OrderDetailEntity;
 import groom.him.domain.order.models.enums.OrderStatus;
 import groom.him.domain.product.models.entity.ProductEntity;
 
-public record OrderProductInfo(
+public record OrderDetailResponse(
     Integer productId,
     String productName,
     String brandName,
     String productImgUrl,
+    Integer orderDetailId,
     Integer price,
     Integer quantity,
     OrderStatus orderStatus,
     String orderDate
 ) {
 
-    public static OrderProductInfo of(OrderDetailEntity entity) {
+    public static OrderDetailResponse of(OrderDetailEntity entity) {
         ProductEntity product = entity.getProduct();
 
-        return new OrderProductInfo(
+        return new OrderDetailResponse(
             product.getProductId(),
             product.getProductName(),
             product.getBrand().getBrandName(),
             product.getImgUrl(),
+            entity.getOrderDetailId(),
             entity.getPrice(),
             entity.getQuantity(),
             entity.getOrderStatus(),
