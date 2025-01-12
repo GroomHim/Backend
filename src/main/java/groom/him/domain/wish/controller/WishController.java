cb261fc139b7069d70b4e09c307645a9a7aeb809
YGwan <good0619@naver.com>
2025-01-12 22:38:11 +0900

    [Refactor] : 위시 관련 생성 & 조회 response 변경

--- src/main/java/groom/him/domain/wish/controller/WishController.java
+++ src/main/java/groom/him/domain/wish/controller/WishController.java
@@ -8,6 +8,7 @@ import groom.him.domain.wish.service.WishService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,14 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/v1/wishes")
 public class WishController {
-
     private final WishService wishService;
 
     @PostMapping
     public Response<Integer> addWish(@AuthenticationPrincipal MemberEntity member,
         @RequestParam Integer productId) {
         wishService.addWish(member.getMemberId(), productId);
-        return Response.success();
+        return new Response<>(HttpStatus.CREATED.value());
     }
 
     @GetMapping
@@ -49,6 +49,6 @@ public class WishController {
     public Response<Integer> deleteWish(@AuthenticationPrincipal MemberEntity member,
         @RequestParam Integer productId) {
         wishService.deleteWish(member.getMemberId(), productId);
-        return Response.success();
+        return new Response<>(HttpStatus.NO_CONTENT.value());
     }
 }
