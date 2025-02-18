0da7313aa0b1f8c8ab97dbc76feb8ebe1537adc1
YooJin919 <jiny08360@gmail.com>
2025-02-18 16:59:00 +0900

    [Chore] 사용하지 않는 import문 삭제

--- src/main/java/groom/him/domain/member/controller/MemberController.java
+++ src/main/java/groom/him/domain/member/controller/MemberController.java
@@ -4,18 +4,13 @@ import groom.him.core.dto.Response;
 import groom.him.domain.member.models.dto.request.ModifyMyInfoRequest;
 import groom.him.domain.member.models.dto.request.ModifyPasswordRequest;
 import groom.him.domain.member.models.dto.request.ValidatePasswordRequest;
-import groom.him.domain.member.models.dto.response.CountResponse;
 import groom.him.domain.member.models.dto.response.MemberResponse;
 import groom.him.domain.member.models.entity.MemberEntity;
 import groom.him.domain.member.service.MemberService;
-import groom.him.domain.wish.service.WishService;
-import groom.him.domain.product.models.dto.response.ProductWithWishResponse;
 import groom.him.domain.qa.models.dto.response.QaResponse;
 import groom.him.domain.qa.models.enums.QaStatus;
 import groom.him.domain.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +22,6 @@ import java.util.List;
 @RequestMapping("/v1/members")
 public class MemberController {
     private final MemberService memberService;
-    private final WishService wishService;
     private final SearchService searchService;
 
     @GetMapping("/info")
