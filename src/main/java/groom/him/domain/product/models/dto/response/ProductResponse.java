855763953e155ca9285cb7eda1e66e81432e9628
YooJin919 <jiny08360@gmail.com>
2025-02-24 21:21:05 +0900

    [Chore] 상품 상세조회시 구매사이트주소 null일 때 반환하지 않도록 수정

--- src/main/java/groom/him/domain/product/models/dto/response/ProductResponse.java
+++ src/main/java/groom/him/domain/product/models/dto/response/ProductResponse.java
@@ -1,5 +1,7 @@
 package groom.him.domain.product.models.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import groom.him.domain.product.models.entity.ProductEntity;
 
 public record ProductResponse(
@@ -11,6 +13,7 @@ public record ProductResponse(
     String brandName,
     String ingredients,
     String deliveryInfo,
+    @JsonInclude(Include.NON_EMPTY)
     String purchaseSiteUrl
 ) {
     public static ProductResponse of(ProductEntity product) {
