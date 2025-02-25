48edbf825df5efe253aba49bd7a9da6e03094398
YooJin919 <jiny08360@gmail.com>
2025-02-25 19:30:14 +0900

    [Feat] 브랜드 유효성 검사 개발

--- src/main/java/groom/him/domain/product/repository/BrandRepository.java
+++ src/main/java/groom/him/domain/product/repository/BrandRepository.java
@@ -1,2 +1,10 @@
-package groom.him.domain.product.repository;public interface BrandRepository {
+package groom.him.domain.product.repository;
+
+import groom.him.domain.product.models.entity.BrandEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<BrandEntity, Integer> {
+    Boolean existsByEnBrandName(String brandName);
 }
--- src/main/java/groom/him/domain/product/service/BrandService.java
+++ src/main/java/groom/him/domain/product/service/BrandService.java
@@ -1,2 +1,19 @@
-package groom.him.domain.product.service;public class BrandService {
+package groom.him.domain.product.service;
+
+import groom.him.domain.product.exception.BrandErrorCode;
+import groom.him.domain.product.exception.BrandException;
+import groom.him.domain.product.repository.BrandRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BrandService {
+    private final BrandRepository brandRepository;
+
+    public void checkBrandExist(String brandName) {
+        if (!brandRepository.existsByEnBrandName(brandName)) {
+            throw new BrandException(BrandErrorCode.BRAND_NOT_EXIST);
+        }
+    }
 }
