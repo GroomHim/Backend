ff526e23e51e064070f744c9633d4200250567c6
YooJin919 <jiny08360@gmail.com>
2025-03-23 23:25:44 +0900

    존재하지 않는 카테고리 오류 Enum 추가

--- src/main/java/groom/him/domain/category/enums/CategoryErrorCode.java
+++ src/main/java/groom/him/domain/category/enums/CategoryErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CategoryErrorCode implements HttpErrorCode {
+    CATEGORY_NOT_EXIST(HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다."),
     CATEGORY_IS_NOT_LEAF(HttpStatus.BAD_REQUEST, "올바르지 않은 카테고리입니다.");
 
     private final HttpStatus httpStatus;
