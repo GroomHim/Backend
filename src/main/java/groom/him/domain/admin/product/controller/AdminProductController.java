8e1997e86a8b2788903a896917cbb17c4423f66d
YooJin919 <jiny08360@gmail.com>
2025-03-23 23:24:47 +0900

    [Feat] ProductExhibitCategoryLinkRepository 생성

---
+++ src/main/java/groom/him/domain/admin/product/controller/AdminProductController.java
@@ -0,0 +1,2 @@
+package groom.him.domain.admin.product.controller;public class AdminProductController {
+}
---
+++ src/main/java/groom/him/domain/admin/product/service/AdminProductService.java
@@ -0,0 +1,2 @@
+package groom.him.domain.admin.product.service;public class AdminProductService {
+}
