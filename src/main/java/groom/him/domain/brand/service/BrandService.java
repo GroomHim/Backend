56b0addfda9ed7cacf5eb395081b8c3ba643f809
YooJin919 <jiny08360@gmail.com>
2025-04-22 18:03:49 +0900

    [Feat] Brand 전체 목록 조회 API 개발

--- src/main/java/groom/him/domain/brand/service/BrandService.java
+++ src/main/java/groom/him/domain/brand/service/BrandService.java
@@ -2,7 +2,10 @@ package groom.him.domain.brand.service;
 
 import groom.him.domain.brand.exception.BrandErrorCode;
 import groom.him.domain.brand.exception.BrandException;
+import groom.him.domain.brand.models.dto.response.BrandResponse;
+import groom.him.domain.brand.models.entity.BrandEntity;
 import groom.him.domain.brand.repository.BrandRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,10 @@ public class BrandService {
         if (!brandRepository.existsByEnBrandName(brandName)) {
             throw new BrandException(BrandErrorCode.BRAND_NOT_EXIST);
         }
+    }
+
+    public List<BrandResponse> findBrandList() {
+        List<BrandEntity> brandEntityList = brandRepository.findAll();
+        return brandEntityList.stream().map(BrandResponse::from).toList();
     }
 }
