81c6304bef1b70e6da8f342c21155e3103244d56
YGwan <good0619@naver.com>
2025-05-05 02:01:27 +0900

    [Feat] ProductBriefResponse에 categoryName 포함

--- src/main/java/groom/him/domain/product/models/dto/response/ProductBriefResponse.java
+++ src/main/java/groom/him/domain/product/models/dto/response/ProductBriefResponse.java
@@ -4,6 +4,7 @@ import groom.him.domain.product.models.entity.ProductEntity;
 
 public record ProductBriefResponse(
     Integer productId,
+    String categoryName,
     String productName,
     String brandName,
     Integer price,
@@ -11,9 +12,11 @@ public record ProductBriefResponse(
     Integer discountedPrice,
     String imgUrl
 ) {
+
     public static ProductBriefResponse of(ProductEntity product) {
         return new ProductBriefResponse(
             product.getProductId(), product.getProductName(), product.getBrand().getBrandName(),
+            product.getCategory().getCategoryName(),
             product.getPrice(), product.getDiscountRate(), product.getDiscountedPrice(),
             product.getImgUrl()
         );
