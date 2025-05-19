1f9ab882523b56f64de9b212cf9c82010c19cde8
YooJin919 <jiny08360@gmail.com>
2025-05-19 19:10:22 +0900

    [Feat] 관리자 페이지 상품 상세 정보 DTO 생성

--- src/main/java/groom/him/domain/admin/product/models/dto/response/AdminProductDetailResponse.java
+++ src/main/java/groom/him/domain/admin/product/models/dto/response/AdminProductDetailResponse.java
@@ -1,2 +1,16 @@
-package groom.him.domain.admin.product.models.dto.response;public record AdminProductDetailResponse() {
+package groom.him.domain.admin.product.models.dto.response;
+
+import groom.him.domain.product.models.dto.response.ProductResponse;
+import java.util.List;
+
+public record AdminProductDetailResponse(
+    ProductResponse product,
+    List<Integer> skinType,
+    List<String> mainImage,
+    List<String> contentImage
+) {
+    public static AdminProductDetailResponse of(ProductResponse product, List<Integer> skinType,
+        List<String> mainImg, List<String> contentImg) {
+        return new AdminProductDetailResponse(product, skinType, mainImg, contentImg);
+    }
 }
